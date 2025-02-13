95f917f6d8ccf68ce3d338db5c26d7e3e67690ad
matheus albuquer <mj898926@gmail.com>
2025-02-13 15:12:14 -0300

    first commit

---
+++ src/main/java/com/projeto/fast_api/FastApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.fast_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/fast_api/FastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.fast_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
